b85828debc6bf3f1e5a7b5b74ce07f543e87b4e8
GaoTF <d_chocolate@163.com>
2019-12-18 17:24:13 +0800

    init

---
+++ src/main/java/com/evil/gaomao/GaomaoApplication.java
@@ -0,0 +1,13 @@
+package com.evil.gaomao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaomaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaomaoApplication.class, args);
+	}
+
+}
